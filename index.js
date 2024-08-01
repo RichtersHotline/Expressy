fef0d1e01e06b1021b03e81adba6e2f06b3a1c02
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-01 15:38:41 +0200

    why.jpg

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-import express from "express"
-import path from "path"
-import {config} from "dotenv"
+import express from 'express'
+import path from 'path'
+import { config } from 'dotenv'
 config()
 const app = express()
 const port = +process.env.PORT || 4000
-const productURL = "http://localhost:3000/results"
+const productURL = 'http://localhost:3000/results'
 app.use(express.static('./static'))
-// get data 
+// Retrieve data
 async function getData() {
-return await (await fetch(productURL)).json()
-
+    return await (await fetch(productURL)).json()
 }
-app.get("/", (req, res) => {
-    
-    res.status(200).sendFile(path.resolve("./static/index.html"))
-
-
+app.get('/', (req, res) => {
+    res.status(200).sendFile(
+        path.resolve('./static/index.html'))
 })
-app.get("/about", (req, res) => {
-    
-    res.status(200).sendFile(path.resolve("./static/about.html"))
-
-
+app.get('/about', (req, res) => {
+    res.status(200).sendFile(
+        path.resolve('./static/about.html'))
 })
-
+app.get('/products', async (req, res) => {
+    try {
+        res.status(200).json(await getData())
+    } catch (e) { 
+        res.status(404).json({
+            msg: 'Please try again late'
+        })
+    }
+})
+app.get('/product/:id', async (req, res) => {
+    try {
+        let data = await getData()
+        const id = +req.params.id
+        if (id < data.length) {
+            res.status(200).json(data[id - 1])
+        } else {
+            res.status(404).json({
+                msg: 'Resource not found'
+            })
+        }
+    } catch (e) { 
+        res.status(404).json({
+            msg: 'Please try again late'
+        })
+    }
+})
 app.listen(port, () => {
-
-    console.log(`Server runs at port ${port}`)
-
+    console.log(`Server is running on ${port}`);
 })
